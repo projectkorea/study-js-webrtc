0f55944591c684801bbd91ba3446c680f8e15c90
김준하 <prodopark15@gmail.com>
2022-05-30 17:54:29 +0900

    feat: websocket 구축

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http'
+import WebSocket, { WebSocketServer } from 'ws'
 import express from 'express'
 
 const app = express()
@@ -11,4 +13,8 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('home'))
 app.get('/*', (req, res) => res.redirect('/'))
 
-app.listen(PORT, handleListen)
+// app.listen(PORT, handleListen)
+const httpServer = http.createServer(app)
+const websocketServer = new WebSocketServer({ server })
+// "ws": "^8.2.3" 버전부터는 Websocket.Server 가 아니라 WebSocketServer 입니다
+server.listen(PORT, handleListen)
